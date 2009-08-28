6c36bebfb0c8b42400dae81b75d96ab162a688ac
Archie J. Brentano <brentaar@gmail.com>
2009-08-27 21:50:30 -0700

    Adding index.php to repo

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<html>
+<head>
+<title>File Download Manager</title>
+<link rel="stylesheet" href="Site.css" />
+</head>
+
+<body class="PageBODY">
+<br /><br />
+<form name="input" action="upload.php" method="post">
+<center>
+<table class="FormTable">
+<tr>
+<td class="FormHeaderTD" colspan=2><center><font class="FormHeaderFONT">FDLM</font></center></td>
+<tr>
+<td class="ColumnTD">What:</td>
+<td class="FieldcaptionTD"><input type="text" name="what" /></td>
+</tr>
+<tr>
+<td class="FieldcaptionTD">md5:</td>
+<td class="FieldcaptionTD"><input type="text" name="file_md5"></td>
+</tr>
+<tr>
+<td class="FieldcaptionTD">Where:</td>
+
+<td class="FieldcaptionTD"><input type="text" name="file_where" /></td>
+</tr>
+<tr>
+<td class="FieldcaptionTD">Dest. File Name</td>
+<td class="FieldcaptionTD"><input type="text" name="file_name1" /></td>
+</tr>
+<tr>
+<td class="FieldcaptionTD" colspan=2><center><input type="submit" value="Submit" /></center></td>
+</tr>
+</table>
+</center>
+</form>
+
+</body>
+</html>
+
